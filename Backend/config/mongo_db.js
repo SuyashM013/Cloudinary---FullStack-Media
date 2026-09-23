c3ea7125c15871cdcd9f70c86c30431e150873c9
Suyash <mishrasuyash013@gmail.com>
2026-09-23 11:15:18 +0530

    Update Mongo error

--- Backend/config/mongo_db.js
+++ Backend/config/mongo_db.js
@@ -5,7 +5,7 @@ const connectDB = async ()=> {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     }catch(err){
-        console.error(err.message);
+        console.error("Mongo Error: ",err.message);
         // process.exit(1);
     }
 
